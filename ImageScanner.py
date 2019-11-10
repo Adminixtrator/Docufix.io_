bbb28e27c026ce9c3a17bea34b42588cd2d8c911
Adminixtratotr <sadiqheritage927@gmail.com>
2019-11-10 18:59:50 +0100

    Updated

--- ImageScanner.py
+++ ImageScanner.py
@@ -1,5 +1,5 @@
-import io
 import os
+import webbrowser as wb
 from flask import Flask, render_template, request, url_for
 import requests
 import pytesseract as pt
@@ -17,7 +17,7 @@ try:
 except:
     print('')
 
-pt.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
+pt.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 #pt.pytesseract.tesseract_cmd = 'Tesseract-OCR/tesseract.exe'
 
 def _get_image(url):
@@ -51,15 +51,23 @@ def allowed_file(filename):
     
 # IMAGE SCANNER
 @app.route('/', methods=['GET', 'POST'])
-def imageScanner():   
+def ImageScanner():   
+
+    about_us=wb.open('docufix.io/about_us.php')
+    fileUpload=wb.open('docufix.io/fileUpload.php')
+    grammerChecker=wb.open('docufix.io/grammerChecker.php')
+    fileDupliate=wb.open('docufix.io/fileDuplicate.php')
+    plagiarismChecker=wb.open('docufix.io/plagiarismChecker.php')
+    paraphrase=wb.open('docufix.io/paraphrase.php')
+
     if request.method == 'POST':
         # check if there is a file in the request
         if 'file1' not in request.files:
-            return render_template('imageScanner.html',)
+            return render_template('ImageScanner.html', about_us=about_us, fileUpload=fileUpload, grammerChecker=grammerChecker, fileDupliate=fileDupliate, plagiarismChecker=plagiarismChecker,)
         file1 = request.files['file1']
         # if no file is selected
         if file1.filename == '':
-            return render_template('imageScanner.html', msg='No file selected')
+            return render_template('ImageScanner.html', msg='No file selected')
 
         if file1 and allowed_file(file1.filename):
 
@@ -67,14 +75,19 @@ def imageScanner():
             extracted_text = ocr_core(file1)
             # Save as text
             os.chdir('static/')
-            f = open('Docufix.txt', 'w', encoding='utf-8')
+            f = open('Docufix.txt', 'w')
             f.write(extracted_text)
             f.close()
             os.chdir('../')
             # Save as docx
             os.chdir('static/')
-            document.add_paragraph(extracted_text)
-            document.save('Docufix.doc')
+            try:
+                document.add_paragraph(extracted_text)
+                document.save('Docufix.doc')
+            except:
+                return render_template('ImageScanner.html',
+                                   msg='Error occured',
+                                   extracted_text=extracted_text)
             os.chdir('../')
             # Save as pdf
             os.chdir('static/')
@@ -87,7 +100,7 @@ def imageScanner():
                 f.output("Docufix.pdf")
             except:
                 extracted_text='Error occured in file encoding..'
-                return render_template('imageScanner.html',
+                return render_template('ImageScanner.html',
                                    msg='Error occured',
                                    extracted_text=extracted_text)
             os.chdir('../')
@@ -97,16 +110,16 @@ def imageScanner():
             else:
                 extracted_text='Could not identify text in image.. \nPlease check image and try again..'
             # extract the text and display it
-            return render_template('imageScanner.html',
+            return render_template('ImageScanner.html',
                                    msg='Successfully processed',
                                    extracted_text=extracted_text)
         else:
-            return render_template('imageScanner.html',
+            return render_template('ImageScanner.html',
                                    msg='Error occured',
                                    extracted_text='Error occured! Please check your file type and try again')
             
     elif request.method == 'GET':
-        return render_template('imageScanner.html')
+        return render_template('ImageScanner.html')
 
 
     
--- README.md
+++ README.md
@@ -1,22 +1,34 @@
-# Docufix.io
+# Image Scanner
+**Just a brief test**
 
-Complete Machine Learning Solution.
-----------------------
+### Ubuntu - Python 2.7.15+
 
-### TESSDATA_PREFIX
-   https://github.com/tesseract-ocr/tesseract/issues/271
-- *Follow the link to compare for ubuntu server..*
- 
- ### TESSERACT_CMD
- ### Manually
-   **Open the terminal and type**
-   
-- ```find -iname tesseract      #path to tesseract_cmd, copy and paste in python file..```
-    
-- ```find -iname tessdata       #path to TESSDATA_PREFIX, set using export TESSDATA_PREFIX=<PATH>```
-  
-- **To Check**
-    ```echo $TESSDATA_PREFIX```
+Fire up your favorite terminal and clone the repo by typing
+   git clone https://github.com/Adminixtrator/Docufix.io_.git
+
+Then move into the Docufix.io_ folder and install requirements using
+   pip install -r requirements.txt
+
+Then run ImageScanner.py
+   python ImageScanner.py
+
+**Setting Tesseract path - Server configuration**
+Install tesseract-ocr _(Already done by requirements.txt)_
+
+Then get tessdata location by typing
+   sudo find / -name "tessdata"
+
+Then copy path and run
+   export TESSDATA_PREFIX=path_copied   _(No Space between)_
+
+Then get tesseract path 
+   sudo find / -name "tesseract"
+
+Copy path and replace **pt.pytesseract.tesseract_cmd** path in _ImageScanner.py_ 
+   pt.pytesseract.tesseract_cmd=path_copied
 
 
+_Go to 127.0.0.1:5000 on your browser and explore.._
+
+### Use later version of browsers to have the most experience..
 
